03c279359b274c066da481a830e36f503a49b23e
shige-e <gramas.e@icloud.com>
2021-09-21 00:51:58 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,11 +9,12 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :image, presence: true
   PRICE_REGEX = /\A[0-9]+\z/
   validates :price, presence: true,
                                format: { with: PRICE_REGEX, message: ' is invalid. Input half-width characters' }
   validates :product_name, :description, presence: true
-  validates :area_id, :arrival_date_id, :catedory_id, :delivery_fee_id, :status_id, presence: true
-  validates :arrival_date_id, :catedory_id, :delivery_fee_id, :status_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :area_id, :arrival_date_id, :category_id, :delivery_fee_id, :status_id, presence: true
+  validates :arrival_date_id, :category_id, :delivery_fee_id, :status_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :area_id, numericality: { other_than: 0, message: "can't be blank" } 
 end
