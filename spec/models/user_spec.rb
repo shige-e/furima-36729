0855b0d90933292ca90d48102f37b0dc2b0eb361
shige-e <gramas.e@icloud.com>
2021-09-20 11:08:11 +0900

    userモデルのテストコード

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,116 @@
+require 'rails_helper'
+ RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe "ユーザー新規登録" do
+    context '新規登録できる場合' do
+      it "全ての項目の入力が存在すれば登録できること" do
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録できない場合' do
+       it "nicknameが空だと登録できない" do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+       end
+
+       it "emailが空では登録できない" do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+       end
+
+       it "passwordが空では登録できない" do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank",
+          "Password  is invalid. Include both letters and numbers")
+      end
+
+      it "family_nameが空では登録できない" do
+        @user.family_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name can't be blank",
+          "Family name  is invalid. Input full-width characters")
+      end
+
+      it "first_nameが空では登録できない" do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank",
+          "First name  is invalid. Input full-width characters")
+      end
+
+      it "first_nameが空では登録できない" do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank",
+          "First name  is invalid. Input full-width characters")
+      end
+
+      it "family_name_kanaが空では登録できない" do
+        @user.family_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name kana can't be blank", 
+          "Family name kana  is invalid. Input full-width katakana characters")
+      end
+
+      it "first_name_kanaが空では登録できない" do
+        @user.first_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana can't be blank", 
+          "First name kana  is invalid. Input full-width katakana characters")
+      end
+
+      it '重複したemailが存在する場合登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '1234567'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it 'passwordが5文字以下であれば登録できない' do
+        @user.password = '12345'
+        @user.password_confirmation = '12345'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+
+      it 'family_nameが全角入力でなければ登録できないこと' do
+        @user.family_name= "ｱｲｳｴｵ"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name  is invalid. Input full-width characters")
+      end
+
+      it 'first_nameが全角入力でなければ登録できないこと' do
+        @user.first_name= "ｱｲｳｴｵ"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name  is invalid. Input full-width characters")
+      end
+
+
+      it 'family_name_kanaが全角カタカナでなければ登録できないこと' do
+        @user.family_name_kana= "あいうえお"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name kana  is invalid. Input full-width katakana characters")
+      end
+
+      it 'first_name_kanaが全角カタカナでなければ登録できないこと' do
+        @user.first_name_kana= "あいうえお"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana  is invalid. Input full-width katakana characters")
+      end
+    end
+  end
+end
