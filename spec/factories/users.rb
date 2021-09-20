9f5ea3271d169972618e5a788ec45aa74c1dbad9
shige-e <gramas.e@icloud.com>
2021-09-20 11:12:28 +0900

    ユーザーのRubocop

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"tarou"}
-    email                 {"sample@gmail.com"}
-    password              {"123456a"}
-    encrypted_password    {"123456a"}
-    family_name           {"山田"}
-    first_name            {"太郎"}
-    family_name_kana      {"ヤマダ"}
-    first_name_kana       {"タロウ"}
-    birth_day             {"2000-01-01"}
+    nickname              { 'tarou' }
+    email                 { 'sample@gmail.com' }
+    password              { '123456a' }
+    encrypted_password    { '123456a' }
+    family_name           { '山田' }
+    first_name            { '太郎' }
+    family_name_kana      { 'ヤマダ' }
+    first_name_kana       { 'タロウ' }
+    birth_day             { '2000-01-01' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,69 +1,69 @@
 require 'rails_helper'
- RSpec.describe User, type: :model do
+RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
 
-  describe "ユーザー新規登録" do
+  describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
-      it "全ての項目の入力が存在すれば登録できること" do
+      it '全ての項目の入力が存在すれば登録できること' do
         expect(@user).to be_valid
       end
     end
 
     context '新規登録できない場合' do
-       it "nicknameが空だと登録できない" do
+      it 'nicknameが空だと登録できない' do
         @user.nickname = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Nickname can't be blank")
-       end
+      end
 
-       it "emailが空では登録できない" do
+      it 'emailが空では登録できない' do
         @user.email = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
-       end
+      end
 
-       it "passwordが空では登録できない" do
+      it 'passwordが空では登録できない' do
         @user.password = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank",
-          "Password  is invalid. Include both letters and numbers")
+                                                      'Password  is invalid. Include both letters and numbers')
       end
 
-      it "family_nameが空では登録できない" do
+      it 'family_nameが空では登録できない' do
         @user.family_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Family name can't be blank",
-          "Family name  is invalid. Input full-width characters")
+                                                      'Family name  is invalid. Input full-width characters')
       end
 
-      it "first_nameが空では登録できない" do
+      it 'first_nameが空では登録できない' do
         @user.first_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name can't be blank",
-          "First name  is invalid. Input full-width characters")
+                                                      'First name  is invalid. Input full-width characters')
       end
 
-      it "first_nameが空では登録できない" do
+      it 'first_nameが空では登録できない' do
         @user.first_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name can't be blank",
-          "First name  is invalid. Input full-width characters")
+                                                      'First name  is invalid. Input full-width characters')
       end
 
-      it "family_name_kanaが空では登録できない" do
+      it 'family_name_kanaが空では登録できない' do
         @user.family_name_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family name kana can't be blank", 
-          "Family name kana  is invalid. Input full-width katakana characters")
+        expect(@user.errors.full_messages).to include("Family name kana can't be blank",
+                                                      'Family name kana  is invalid. Input full-width katakana characters')
       end
 
-      it "first_name_kanaが空では登録できない" do
+      it 'first_name_kanaが空では登録できない' do
         @user.first_name_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name kana can't be blank", 
-          "First name kana  is invalid. Input full-width katakana characters")
+        expect(@user.errors.full_messages).to include("First name kana can't be blank",
+                                                      'First name kana  is invalid. Input full-width katakana characters')
       end
 
       it '重複したemailが存在する場合登録できない' do
@@ -88,28 +88,27 @@ require 'rails_helper'
       end
 
       it 'family_nameが全角入力でなければ登録できないこと' do
-        @user.family_name= "ｱｲｳｴｵ"
+        @user.family_name = 'ｱｲｳｴｵ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family name  is invalid. Input full-width characters")
+        expect(@user.errors.full_messages).to include('Family name  is invalid. Input full-width characters')
       end
 
       it 'first_nameが全角入力でなければ登録できないこと' do
-        @user.first_name= "ｱｲｳｴｵ"
+        @user.first_name = 'ｱｲｳｴｵ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name  is invalid. Input full-width characters")
+        expect(@user.errors.full_messages).to include('First name  is invalid. Input full-width characters')
       end
 
-
       it 'family_name_kanaが全角カタカナでなければ登録できないこと' do
-        @user.family_name_kana= "あいうえお"
+        @user.family_name_kana = 'あいうえお'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family name kana  is invalid. Input full-width katakana characters")
+        expect(@user.errors.full_messages).to include('Family name kana  is invalid. Input full-width katakana characters')
       end
 
       it 'first_name_kanaが全角カタカナでなければ登録できないこと' do
-        @user.first_name_kana= "あいうえお"
+        @user.first_name_kana = 'あいうえお'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name kana  is invalid. Input full-width katakana characters")
+        expect(@user.errors.full_messages).to include('First name kana  is invalid. Input full-width katakana characters')
       end
     end
   end
