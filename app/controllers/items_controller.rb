a6b5e6305b50a94211bfc9f91609e03841dfccc0
shige-e <gramas.e@icloud.com>
2021-09-21 17:40:01 +0900

    出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :destroy, :new]
+
   def index
+    @items = Item.all
   end
 
   def new
@@ -7,6 +10,19 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
+
+  private
+
+  def item_params
+    params.require(:item).
+    permit(:image, :price, :product_name, :description,:area_id, :arrival_date_id, :category_id, :delivery_fee_id, :status_id).
+    merge(user_id: current_user.id)
+  end
 end
